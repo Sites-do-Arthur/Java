6459c1bb3e036dafd76d11b462f8c88d89ba61d5
ArthurHenriqueSchlikmann <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2023-07-29 18:29:55 -0300

    v5

--- CalculadoraInterativa2A/src/main/java/com/sitesdoarthur/calculadorainterativa2a/CalculadoraInterativa2A.java
+++ CalculadoraInterativa2A/src/main/java/com/sitesdoarthur/calculadorainterativa2a/CalculadoraInterativa2A.java
@@ -26,5 +26,12 @@ public class CalculadoraInterativa2A {
         idade = teclado.nextByte();
         System.out.printf("%S de " + idade + " anos, escolha uma opção abaixo: \n", nome);
         System.out.println("Digite [1] para Adição");
+        System.out.println("Digite [2] para Subtração");
+        System.out.printf("%S, caso você queira mais opções baixe agora a Calculadora Interativa 2B ou 2A+, \n", nome);
+        System.out.printf("a Calculadora Interativa 2A não tem suporte para outras opções, em caso de duvida \n");
+        System.out.printf("consulte mais informações em https://sites-do-arthur.github.io/calculadora-interariva/sobre.html \n");
+        System.out.println("E precione [ENTER] no fim da escolha para comfirmar");
+        menu = teclado.nextByte();
+        
     }
 }
