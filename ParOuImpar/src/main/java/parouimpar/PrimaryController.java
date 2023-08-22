4d1ae33f4ada846186da3a380f1c076d6f70500f
ArthurHenriqueSchlikmann <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2023-08-21 23:01:42 -0300

    v11

--- ParOuImpar/src/main/java/parouimpar/PrimaryController.java
+++ ParOuImpar/src/main/java/parouimpar/PrimaryController.java
@@ -1,12 +1,24 @@
 package parouimpar;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextField valor;
+    private Button clique;
+    private Label resultado;
+    
+    @FXML
+    private void clikou() {
+        int numero = Integer.parseInt(valor.getText());
+        if(numero % 2 == 0) {
+            resultado.setText("O valor " + numero + " é PAR.");
+        } else {
+            resultado.setText("O valor " + numero + " é IMPAR");
+        }
     }
 }
