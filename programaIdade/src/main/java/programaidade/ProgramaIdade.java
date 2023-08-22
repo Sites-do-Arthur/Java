57146081bbcdd0da34129600d3cfe7c057134713
ArthurHenriqueSchlikmann <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2023-08-21 21:30:53 -0300

    -a

---
+++ programaIdade/src/main/java/programaidade/ProgramaIdade.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package programaidade;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Win10
+ * 
+ * @version 1b
+ */
+public class ProgramaIdade {
+
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Iniciando CP-GUI");
+        System.out.print("Em que ano você naceu: ");
+        int an = teclado.nextInt();
+        System.out.print("Em que ano estamos");
+        int a = teclado.nextInt();
+        int idade = a - an;
+        
+    }
+}
