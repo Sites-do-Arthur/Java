37e373743cef2ea4f7fa3bdf665218470c7c6c51
ArthurHenriqueSchlikmann <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2023-08-24 20:30:13 -0300

    v21

--- SystemCP-g/src/main/java/systemcp/g/ProgramManagerController.java
+++ SystemCP-g/src/main/java/systemcp/g/ProgramManagerController.java
@@ -4,12 +4,14 @@
  */
 package systemcp.g;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
+import systemcp.g.sql.java.App;
 
 /**
  * FXML Controller class
@@ -23,6 +25,13 @@ public class ProgramManagerController implements Initializable {
     private RadioButton File;
     private Button Aplic;
     private Button Close;
+    
+    @FXML
+    private void ativar() throws IOException {
+        if(Calc.isSelected()) {
+            App.setRoot("CalculadoraInterativa.fxml");
+        }
+    }
     
     /**
      * Initializes the controller class.
